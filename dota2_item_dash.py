f349f5b51e8557b89b7e45fb39e02a99884cafab
luswangl2013 <34317798+luswangl2013@users.noreply.github.com>
2023-10-17 13:57:46 -0600

    Create dota2_item_dash.py

---
+++ dota2_item_dash.py
@@ -0,0 +1,74 @@
+import pandas as pd
+from dash import Dash, html
+import dash_cytoscape as cyto
+
+df = pd.read_csv("./item_test.csv")
+
+print(df)
+app = Dash(__name__)
+
+nodes = [
+    {
+        'data': {'id': item, 'label': item, 'category': category}
+    }
+    for item, category in (
+        list(df[['item', 'category']].itertuples(index=False, name=None))
+    )
+]
+df_edge = df.dropna()[['item', 'next']]
+edges = [
+    {'data': {'source': item, 'target': next}}
+    for item, next in (
+        list(df_edge.itertuples(index=False, name=None))
+    )
+]
+elements = nodes + edges
+
+app.layout = html.Div([
+    cyto.Cytoscape(
+        id='cytoscape-two-nodes',
+        layout={'name': 'cose'},
+        style={'width': '100%', 'height': '1000px'},
+        elements=elements,
+        stylesheet=[{
+            'selector': 'edge',
+                'style': {
+                    'curve-style': 'bezier',
+                    'target-arrow-color': 'blue',
+                    'target-arrow-shape': 'vee',
+                    'line-color': 'blue'
+                }
+        },
+        {
+                'selector': 'node',
+                'style': {
+                    'label': 'data(label)'
+                }
+            },
+            {
+                'selector': '[category *= "a"]',
+                'style': {
+                    'background-color': '#6495ED'
+                    #'shape': 'rectangle'
+                }
+            },
+            {
+                'selector': '[category *= "b"]',
+                'style': {
+                    'background-color': '#FF4136'
+                    #'shape': 'rectangle'
+                }
+            },
+            {
+                'selector': '[category *= "c"]',
+                'style': {
+                   'background-color': '#DFFF00'
+                    #'shape': 'rectangle'
+                }
+            },
+            ]
+    )
+])
+
+if __name__ == '__main__':
+    app.run(debug=True)
