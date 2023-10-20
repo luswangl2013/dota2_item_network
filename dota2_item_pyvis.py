d98987cc1db56b9459e91f54755d3795acbc1220
luswangl2013 <34317798+luswangl2013@users.noreply.github.com>
2023-10-20 14:57:21 -0600

    update pyvis

--- dota2_item_pyvis.py
+++ dota2_item_pyvis.py
@@ -6,7 +6,7 @@ df = pd.read_csv("./dota2_7.33_item.csv")
 
 print(df)
 
-net = Network(notebook=False,directed=True,height='1200px')
+net = Network(notebook=False,directed=True,height='800px',select_menu=True,filter_menu=True)
 df_nodes = df[['item', 'category']].drop_duplicates()
 color_dict = {"basic_attributes": "#fb4b4b", 
               "basic_equipment": "#fb874b", 
@@ -28,5 +28,16 @@ edge_list = list(df_edge.itertuples(index=False, name=None))
 
 net.add_edges(edge_list)
 
+neighbor_map = net.get_adj_list()
+
+# add neighbor data to node hover data
+for node in net.nodes:
+                node["title"] = node["label"]
+                node["title"] += " Neighbors:\n" + "\n".join(neighbor_map[node["id"]])
+                node["value"] = len(neighbor_map[node["id"]])
+                node["group"] = df_nodes.loc[df_nodes['item'] == node["label"]]['category'].values[0]
+                node["level"] = node["group"].split('_')[0]
+
 net.barnes_hut()
+net.show_buttons(filter_=['physics'])
 net.show("example.html", notebook=False)
